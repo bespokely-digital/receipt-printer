4bbd09e94542d1ef3111dbf9fcac7020ece5c301
Michael Chalmers <michael@bespokely.cc>
2024-11-07 16:27:14 +0000

    refactor to simplify

--- app.js
+++ app.js
@@ -1,12 +1,10 @@
 import express from 'express';
-import printRoutes from './routes/printRoutes.js';
 
 const app = express();
 const PORT = 3000;
 
 // Middleware
 app.use(express.json());
-// app.use('/api', printRoutes);
 app.use(express.static('public')); // Serve static files from "public" directory
 
 app.listen(PORT, () => {
--- routes/printRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { connectPrinter, printReceipt } from '../controllers/printController.js';
-
-const router = express.Router();
-
-router.post('/connect', connectPrinter); // New route to connect to the printer
-router.post('/print', printReceipt);     // Existing route to print
-
-export default router;
